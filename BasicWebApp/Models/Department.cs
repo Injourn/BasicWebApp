de2fad26b70e1fdbaac2458f91a7215fef1ab9ab
Scott-GH <thatguywhowas@live.com>
2020-11-01 15:07:30 -0800

    Added Models

---
+++ BasicWebApp/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicWebApp.Models {
+    public class Department {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
