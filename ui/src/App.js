73f6c1de5c894d378b3e31b75803c52696575b1e
Scott-GH <thatguywhowas@live.com>
2020-11-18 13:33:25 -0800

    added forms to pages and changed background

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,6 +5,8 @@ import Navbar from './NavBar';
 import {Route, Switch} from 'react-router-dom'
 import EmployeeList from './Employee/EmployeeList';
 import {useLocation} from 'react-router-dom';
+import DepartmentForm from './department/DepartmentForm';
+import EmployeeForm from './Employee/EmployeeForm';
 
 // TODO : Update API Change "Date" to "DateOfJoining"
 function App() {
@@ -15,8 +17,17 @@ function App() {
       <div>        
         <Switch>
           <Route exact path='/' component={ExampleComponent}/>
-          <Route path='/Department' component={Departments}/>
-          <Route path='/Employees' component={EmployeeList}/>
+          <Route path='/Department' component={props =>
+            <div>
+              <Departments/>
+              <DepartmentForm/>
+            </div>}/>
+          <Route path='/Employees' component={props =>
+            <div>
+              <EmployeeList/>
+              <EmployeeForm/>
+            </div>
+          }/>
         </Switch>        
       </div>
     </div>
